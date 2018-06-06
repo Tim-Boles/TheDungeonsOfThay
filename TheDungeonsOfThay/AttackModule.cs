4bb71bd85c3034aa5772837afff82a3a41273d47
TDM_Jester <Elijah.schmidt318@gmail.com>
2018-06-05 22:49:28 -0400

    Added barebones AttackModule, testing to come!

---
+++ TheDungeonsOfThay/AttackModule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDungeonsOfThay
+{
+    class AttackModule
+    {
+        Random Dice;
+
+        public float _DamageMod;
+        public float _DamageStat;
+        public float _HitChance;
+
+        public AttackModule(float dmgMod, float dmgStat)
+        {
+             _DamageMod = dmgMod;
+             _DamageStat = dmgStat;
+
+            Dice = new Random();
+
+            _HitChance = dmgStat / 10;
+
+        }
+     
+
+        public float ATKPowerCalc()
+        {   
+           
+
+            return _DamageMod + _DamageStat;
+
+        }
+
+        public float ChanceToHitCalc()
+        {
+          return _HitChance + Dice.Next(1, 21);
+            
+        }
+
+    }
+}
